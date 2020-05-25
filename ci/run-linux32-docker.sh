b9565a201f5f49e477f11745575f0e8ec1da878e
SZEDER Gábor <szeder.dev@gmail.com>
2020-05-25 08:32:13 +0200

    Travis CI build adjustments
    split-index: more BUG()s
    ci: use Coccinelle 1.0.8 in the static analysis CI jobs
    travis-ci: use Coccinelle v1.0.7 in the static analysis build job
    ci: show how long it takes to run each 'make' and 'make test'
    [PoC] travis-ci: split the test suite in the OSX build jobs
    travis-ci: "unroll" the build matrix
    travis-ci: remove empty 'addons:' sections
    ci: install dependencies to fulfill a few more prereqs
    travis-ci: use a custom Docker image in the 32 bit Linux build job
    travis-ci: build Git with 'make -k'
    tests: show missed prereqs at the end of 'make test'
    [PoC] ci: avoid 'brew' in the OSX CI jobs
    ci: don't 'brew link' gettext
    travis-ci: build and test on s390x
    t5324: don't check progress output in octopus test
    ci: run GETTEXT POISON build job in scrambled mode, too
    gettext: introduce GIT_TEST_GETTEXT_POISON_SCRAMBLED
    gettext: use an enum for the mode of GETTEXT POISONing
    gettext: put "# GETTEXT POISON #" string literal into a macro
    lib-rebase: loosen GETTEXT_POISON check in fake editor
    travis-ci: make short test paths & trash dir in log work together
    travis-ci: run tests in shorter paths
    test-lib: add the '--short-trash-dir' option
    travis-ci: squelch 'prove' progress indicator
    Revert "ci: make MAKEFLAGS available inside the Docker container in the Linux32 job"
    Revert "ci/lib-docker: preserve required environment variables"
    Revert "ci/linux32: parameterise command to switch arch"
    Revert "ci: refactor docker runner script"
    Revert "ci/linux32: libify install-dependencies step"
    Revert "travis: build and test on Linux with musl libc and busybox"
    Revert "ci/lib: if CI type is unknown, show the environment variables"
    Revert "ci/lib: allow running in GitHub Actions"
    Revert "ci/lib: set TERM environment variable if not exist"
    Revert "ci: fix the `jobname` of the `GETTEXT_POISON` job"
    Revert "ci: explicit install all required packages"
    Revert "ci: run gem with sudo to install asciidoctor"
    Revert "ci: configure GitHub Actions for CI/PR"
    Revert "README: add a build badge for the GitHub Actions runs"
    Revert "ci: retire the Azure Pipelines definition"
    Revert "tests: when run in Bash, annotate test failures with file name/line number"
    Revert "ci: add a problem matcher for GitHub Actions"
    Revert "ci: let GitHub Actions upload failed tests' directories"
    Revert "ci: allow per-branch config for GitHub Actions"
    Revert "t/test_lib: avoid naked bash arrays in file_lineno"

---
+++ ci/run-linux32-docker.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Download and run Docker image to build and test 32-bit Git
+#
+
+. ${0%/*}/lib.sh
+
+# Use the following command to debug the docker build locally:
+# $ docker run -itv "${PWD}:/usr/src/git" --entrypoint /bin/bash daald/ubuntu32:xenial
+# root@container:/# /usr/src/git/ci/run-linux32-build.sh <host-user-id>
+
+container_cache_dir=/tmp/travis-cache
+
+MAKEFLAGS=-j2
+docker run \
+	--interactive --tty \
+	--env MAKEFLAGS \
+	--env DEVELOPER \
+	--env DEFAULT_TEST_TARGET \
+	--env GIT_PROVE_OPTS \
+	--env GIT_TEST_OPTS \
+	--env GIT_TEST_CLONE_2GB \
+	--env cache_dir="$container_cache_dir" \
+	--env HOME=/home/ci \
+	--volume "${PWD}:/usr/src/git" \
+	--volume "$cache_dir:$container_cache_dir" \
+	szeder/ubuntu32-for-git-ci:16.04-1 \
+	/usr/src/git/ci/run-linux32-build.sh $(id -u $USER)
+
+check_unignored_build_artifacts
+
+save_good_tree

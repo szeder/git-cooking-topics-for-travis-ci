2304eeff167c9a5ccbf19685cb3bb1b9eb6605ca
SZEDER Gábor <szeder.dev@gmail.com>
2020-07-08 23:10:18 +0200

    Travis CI build adjustments
    split-index: more BUG()s
    ci: use Coccinelle 1.0.8 in the static analysis CI jobs
    travis-ci: use Coccinelle v1.0.7 in the static analysis build job
    ci: show how long it takes to run each 'make' and 'make test'
    [PoC] travis-ci: split the test suite in the OSX build jobs
    travis-ci: "unroll" the build matrix
    travis-ci: remove empty 'addons:' sections
    ci: install dependencies to fulfill a few more prereqs
    travis-ci: use a custom Docker image in the 32 bit Linux build job
    travis-ci: build Git with 'make -k'
    tests: show missed prereqs at the end of 'make test'
    [PoC] ci: avoid 'brew' in the OSX CI jobs
    ci: don't 'brew link' gettext
    travis-ci: build and test on s390x
    t5324: don't check progress output in octopus test
    ci: run GETTEXT POISON build job in scrambled mode, too
    gettext: introduce GIT_TEST_GETTEXT_POISON_SCRAMBLED
    gettext: use an enum for the mode of GETTEXT POISONing
    gettext: put "# GETTEXT POISON #" string literal into a macro
    lib-rebase: loosen GETTEXT_POISON check in fake editor
    travis-ci: make short test paths & trash dir in log work together
    travis-ci: run tests in shorter paths
    test-lib: add the '--short-trash-dir' option
    travis-ci: squelch 'prove' progress indicator
    Revert "ci: make MAKEFLAGS available inside the Docker container in the Linux32 job"
    Revert "ci/lib-docker: preserve required environment variables"
    Revert "ci/linux32: parameterise command to switch arch"
    Revert "ci: refactor docker runner script"
    Revert "ci/linux32: libify install-dependencies step"
    Revert "travis: build and test on Linux with musl libc and busybox"
    Revert "ci/lib: if CI type is unknown, show the environment variables"
    Revert "ci/lib: allow running in GitHub Actions"
    Revert "ci/lib: set TERM environment variable if not exist"
    Revert "ci: fix the `jobname` of the `GETTEXT_POISON` job"
    Revert "ci: explicit install all required packages"
    Revert "ci: run gem with sudo to install asciidoctor"
    Revert "ci: configure GitHub Actions for CI/PR"
    Revert "README: add a build badge for the GitHub Actions runs"
    Revert "ci: retire the Azure Pipelines definition"
    Revert "tests: when run in Bash, annotate test failures with file name/line number"
    Revert "ci: add a problem matcher for GitHub Actions"
    Revert "ci: let GitHub Actions upload failed tests' directories"
    Revert "ci: allow per-branch config for GitHub Actions"
    Revert "ci/config: correct instruction for CI preferences"
    Revert "t/test_lib: avoid naked bash arrays in file_lineno"
    Revert "ci: avoid pounding on the poor ci-artifacts container"
    Revert "Revert "t/test_lib: avoid naked bash arrays in file_lineno""
    Revert "Revert "ci: add a problem matcher for GitHub Actions""
    Revert "Revert "tests: when run in Bash, annotate test failures with file name/line number""

--- ci/run-linux32-build.sh
+++ ci/run-linux32-build.sh
@@ -1,33 +1,18 @@
 #!/bin/sh
 #
-# Build and test Git inside container
+# Build and test Git in a 32-bit environment
 #
 # Usage:
-#   run-docker-build.sh <host-user-id>
+#   run-linux32-build.sh <host-user-id>
 #
 
 set -ex
 
 if test $# -ne 1 || test -z "$1"
 then
-	echo >&2 "usage: run-docker-build.sh <host-user-id>"
+	echo >&2 "usage: run-linux32-build.sh <host-user-id>"
 	exit 1
 fi
-
-case "$jobname" in
-Linux32)
-	switch_cmd="linux32 --32bit i386"
-	;;
-linux-musl)
-	switch_cmd=
-	useradd () { adduser -D "$@"; }
-	;;
-*)
-	exit 1
-	;;
-esac
-
-"${0%/*}/install-docker-dependencies.sh"
 
 # If this script runs inside a docker container, then all commands are
 # usually executed as root. Consequently, the host user might not be
@@ -58,18 +43,12 @@ else
 	test -n "$cache_dir" && chown -R $HOST_UID:$HOST_UID "$cache_dir"
 fi
 
-# Build and test
-command $switch_cmd su -m -l $CI_USER -c "
+su -m -l $CI_USER -c '
 	set -ex
-	export DEVELOPER='$DEVELOPER'
-	export DEFAULT_TEST_TARGET='$DEFAULT_TEST_TARGET'
-	export GIT_PROVE_OPTS='$GIT_PROVE_OPTS'
-	export GIT_TEST_OPTS='$GIT_TEST_OPTS'
-	export GIT_TEST_CLONE_2GB='$GIT_TEST_CLONE_2GB'
-	export MAKEFLAGS='$MAKEFLAGS'
-	export cache_dir='$cache_dir'
 	cd /usr/src/git
-	test -n '$cache_dir' && ln -s '$cache_dir/.prove' t/.prove
-	make
-	make test
-"
+	test -n "$cache_dir" && ln -s "$cache_dir/.prove" t/.prove
+	make -k
+	mkfifo .git/prove-output
+	cat .git/prove-output &
+	make test >.git/prove-output
+'

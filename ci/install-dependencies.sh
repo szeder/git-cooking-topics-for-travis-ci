c0a973748bbebea4e567ee77cda8934971ccca66
SZEDER Gábor <szeder.dev@gmail.com>
2020-07-21 08:23:36 +0200

    Travis CI build adjustments
    split-index: more BUG()s
    ci: use Coccinelle 1.0.8 in the static analysis CI jobs
    travis-ci: use Coccinelle v1.0.7 in the static analysis build job
    [RFC] travis-ci: remove bogus 'pyenv' in the Linux jobs
    ci: show how long it takes to run each 'make' and 'make test'
    [PoC] travis-ci: split the test suite in the OSX build jobs
    travis-ci: "unroll" the build matrix
    travis-ci: remove empty 'addons:' sections
    ci: install dependencies to fulfill a few more prereqs
    travis-ci: use a custom Docker image in the 32 bit Linux build job
    travis-ci: build Git with 'make -k'
    tests: show missed prereqs at the end of 'make test'
    [PoC] ci: avoid 'brew' in the OSX CI jobs
    ci: don't 'brew link' gettext
    travis-ci: build and test on s390x
    t5324: don't check progress output in octopus test
    ci: run GETTEXT POISON build job in scrambled mode, too
    gettext: introduce GIT_TEST_GETTEXT_POISON_SCRAMBLED
    gettext: use an enum for the mode of GETTEXT POISONing
    gettext: put "# GETTEXT POISON #" string literal into a macro
    lib-rebase: loosen GETTEXT_POISON check in fake editor
    travis-ci: make short test paths & trash dir in log work together
    travis-ci: run tests in shorter paths
    test-lib: add the '--short-trash-dir' option
    travis-ci: squelch 'prove' progress indicator

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -12,7 +12,8 @@ case "$jobname" in
 linux-clang|linux-gcc)
 	sudo apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
 	sudo apt-get -q update
-	sudo apt-get -q -y install language-pack-is libsvn-perl apache2
+	sudo apt-get -q -y install language-pack-is libsvn-perl apache2 time \
+		acl libio-pty-perl libjson-perl libhttp-date-perl
 	case "$jobname" in
 	linux-gcc)
 		sudo apt-get -q -y install gcc-8
@@ -32,34 +33,38 @@ linux-clang|linux-gcc)
 		tar --extract --gunzip --file "git-lfs-linux-amd64-$LINUX_GIT_LFS_VERSION.tar.gz"
 		cp git-lfs-$LINUX_GIT_LFS_VERSION/git-lfs .
 	popd
+
+	if test true = "$TRAVIS" &&
+	   pyenv_root=$(pyenv root)
+	then
+		# pyenv in Travis CI's current default (xenial) Linux
+		# image messes up PATH for 'git p4'.
+		sudo rm -rf "$pyenv_root"
+	fi
 	;;
 osx-clang|osx-gcc)
-	export HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_CLEANUP=1
 	# Uncomment this if you want to run perf tests:
 	# brew install gnu-time
-	test -z "$BREW_INSTALL_PACKAGES" ||
-	brew install $BREW_INSTALL_PACKAGES
-	brew link --force gettext
-	brew cask install --no-quarantine perforce || {
-		# Update the definitions and try again
-		cask_repo="$(brew --repository)"/Library/Taps/homebrew/homebrew-cask &&
-		git -C "$cask_repo" pull --no-stat &&
-		brew cask install --no-quarantine perforce
-	} ||
-	brew install caskroom/cask/perforce
-	case "$jobname" in
-	osx-gcc)
-		brew install gcc@9
-		# Just in case the image is updated to contain gcc@9
-		# pre-installed but not linked.
-		brew link gcc@9
-		;;
-	esac
+	(
+		cd ~
+		ver=$(sw_vers | sed -n -e 's/^ProductVersion:[[:space:]]*\([0-9]*\.[0-9]*\)\..*/\1/p')
+		git clone --branch=ci-deps-osx-$ver --single-branch --depth=1 \
+			https://github.com/szeder/git deps
+		cd deps
+		./install.sh
+	)
+	;;
+s390x)
+	sudo apt-get -q update
+	sudo apt-get -q -y install libcurl4-openssl-dev libssl-dev \
+		libexpat-dev gettext language-pack-is libsvn-perl apache2 \
+		acl libio-pty-perl libjson-perl libhttp-date-perl time
+	;;
+Linux32)
+	docker pull szeder/ubuntu32-for-git-ci:16.04-1
 	;;
 StaticAnalysis)
-	sudo apt-get -q update
-	sudo apt-get -q -y install coccinelle libcurl4-openssl-dev libssl-dev \
-		libexpat-dev gettext
+	docker pull szeder/coccinelle:1.0.8-1
 	;;
 Documentation)
 	sudo apt-get -q update
